dea4486d444c94b62d5a165a9851f90c062f08e4
rony1533 <ronysobral5@gmail.com>
2024-12-01 18:46:42 -0300

    feat: adicionando condicao se o rabbitmq esta ativo

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.6"
 }
+
 val springCloudVersion by extra("2023.0.3")
 val springdocVersion = "2.6.0"
 val mapstructVersion = "1.6.3"
@@ -14,12 +15,6 @@ version = "1.0.0"
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
 	}
 }
 
@@ -35,20 +30,27 @@ dependencies {
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
-	implementation("org.springframework.cloud:spring-cloud-starter-stream-rabbit")
-	implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
 	implementation("org.mapstruct:mapstruct:$mapstructVersion")
+
+	// Adicionando uma verificação para o RabbitMQ no ambiente de desenvolvimento
+	if (project.hasProperty("rabbitmqEnabled") && project.property("rabbitmqEnabled") == "true") {
+		implementation("org.springframework.cloud:spring-cloud-starter-stream-rabbit")
+		implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
+	}
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
+
 	annotationProcessor("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:$mapstructLombokBindingVersion")
 	annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.cloud:spring-cloud-stream-test-support")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.0")
-
 }
+
 dependencyManagement {
 	imports {
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
