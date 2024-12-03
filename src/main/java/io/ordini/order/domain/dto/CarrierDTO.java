f39dedec1279cfa42cc749536adb4aa57a3883fd
rony1533 <ronysobral5@gmail.com>
2024-12-02 21:33:51 -0300

    feat: adicionando o carrierDTO

---
+++ src/main/java/io/ordini/order/domain/dto/CarrierDTO.java
@@ -0,0 +1,64 @@
+package io.ordini.order.domain.dto;
+
+import java.util.UUID;
+
+public class CarrierDTO {
+
+    private UUID id;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String address;
+
+    public CarrierDTO(UUID id, String name, String phone, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
