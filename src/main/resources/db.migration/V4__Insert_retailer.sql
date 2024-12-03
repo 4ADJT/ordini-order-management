d40a6f1c77900e50c47a3c3bb1fa2edbb0185825
rony1533 <ronysobral5@gmail.com>
2024-12-03 20:31:21 -0300

    feat: adicionando o insert

---
+++ src/main/resources/db.migration/V4__Insert_retailer.sql
@@ -0,0 +1,6 @@
+INSERT INTO retailer (retailer_name, lat, lon)
+VALUES
+    ('Kabum', '-23.550520', '-46.633308'),
+    ('Magalu', '-22.906847', '-43.172896'),
+    ('Amazon', '-19.916681', '-43.934493'),
+    ('Casas Bahia', '-12.971399', '-38.501392');
