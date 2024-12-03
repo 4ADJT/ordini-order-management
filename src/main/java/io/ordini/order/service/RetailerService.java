a05cc563c7aaeffcf9f10027382852da3c0d3c34
rony1533 <ronysobral5@gmail.com>
2024-12-02 21:35:36 -0300

    feat: adicionando o servicos do revendedor

---
+++ src/main/java/io/ordini/order/service/RetailerService.java
@@ -0,0 +1,44 @@
+package io.ordini.order.service;
+
+import io.ordini.order.controller.exception.OrderException;
+import io.ordini.order.domain.model.RetailerModel;
+import io.ordini.order.repositories.RetailerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class RetailerService {
+
+    private RetailerRepository retailerRepository;
+
+    public List<RetailerModel> getRetailer() {
+        return retailerRepository.findAll();
+    }
+
+    public RetailerModel getRetailerById(UUID id) {
+        return retailerRepository.findById(id)
+                .orElseThrow(() -> new OrderException("Revendedor não encontrado.", HttpStatus.NOT_FOUND));
+    }
+
+    public RetailerModel createRetailer(RetailerModel retailer) {
+        return retailerRepository.save(retailer);
+    }
+
+    public RetailerModel updateRetailer(UUID id,RetailerModel retailer) {
+        RetailerModel retailerResult = retailerRepository.getReferenceById(id);
+        retailerResult.setRetailer_name(retailer.getRetailer_name());
+        retailerResult.setLat(retailer.getLat());
+        retailerResult.setLon(retailer.getLon());
+
+        return retailerRepository.save(retailerResult);
+
+    }
+
+    public void deleteRetailer(UUID id) {
+        retailerRepository.deleteById(id);
+    }
+
+}
