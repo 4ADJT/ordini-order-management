00c2a9434b27d5e80d47700277bb2d4d96c52781
rony1533 <ronysobral5@gmail.com>
2024-12-02 21:31:57 -0300

    feat: adicionando o feign do delivery

---
+++ src/main/java/io/ordini/order/openfeign/DeliveryClient.java
@@ -0,0 +1,21 @@
+package io.ordini.order.openfeign;
+
+import io.ordini.order.domain.dto.CarrierDTO;
+import io.ordini.order.domain.dto.DeliveryRequestDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "delivery", url = "http://logistics/api/logistics")
+public interface DeliveryClient {
+
+    @PostMapping("/deliveries")
+    ResponseEntity<DeliveryRequestDTO> createDelivery(@RequestBody DeliveryRequestDTO deliveryRequest);
+
+    @GetMapping("/carriers")
+    ResponseEntity<List<CarrierDTO>> getAllCarriers();
+}
